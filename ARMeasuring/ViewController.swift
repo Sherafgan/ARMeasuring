b8f81ca07adcbef1726558626868555ce5eb96da
Sherafgan <Kandov.Sh.S@sberbank.ru>
2018-10-25 21:25:42 +0300

    added distance measurement test app

--- ARMeasuring/ViewController.swift
+++ ARMeasuring/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var zLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var distance: UILabel!
+    var startingPosition: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+        
+//        self.distance.layer.masksToBounds = true
+//        self.distance.layer.cornerRadius = 0.8
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+         self.sceneView.delegate = self
     }
-
-
+    
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        guard let sceneView = sender.view as? ARSCNView else {return}
+        guard let currentFrame = sceneView.session.currentFrame else {return}
+        if self.startingPosition != nil {
+            self.startingPosition?.removeFromParentNode()
+            self.startingPosition = nil
+            return
+        }
+        let camera = currentFrame.camera
+        let transform = camera.transform
+        var translationMatrix = matrix_identity_float4x4
+        translationMatrix.columns.3.z = -0.1
+        let modifiedMatrix = simd_mul(transform, translationMatrix)
+        let sphere = SCNNode(geometry: SCNSphere(radius: 0.005))
+        sphere.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        sphere.simdTransform = modifiedMatrix
+        self.sceneView.scene.rootNode.addChildNode(sphere)
+        self.startingPosition = sphere
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        guard let startingPosition = self.startingPosition else {return}
+        guard let pointOfView = self.sceneView.pointOfView else {return}
+        let transform = pointOfView.transform
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let xDistance = location.x - startingPosition.position.x
+        let yDistance = location.y - startingPosition.position.y
+        let zDistance = location.z - startingPosition.position.z
+        DispatchQueue.main.async {
+            self.xLabel.text = String(format: "%.2f", xDistance) + "m"
+            self.yLabel.text = String(format: "%.2f", yDistance) + "m"
+            self.zLabel.text = String(format: "%.2f", zDistance) + "m"
+            self.distance.text = String(format: "%.2f", self.distanceTravelled(x: xDistance, y: yDistance, z: zDistance)) + "m"
+        }
+    }
+    
+    func distanceTravelled(x: Float, y: Float, z: Float) -> Float {
+        return (sqrtf(x*x + y*y + z*z))
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,5 @@
+##### ARMeasuring 
+Distance measurement (tryout) app screenshots
+
+![](images/distance1.png?raw=true "Before tap on the screen")
+![](images/distance2.png?raw=true "After tap on the screen")
